6a6f8c3dc6ed400629127142dc8d71ba13cb56e8
kenway214 <kenway214@outlook.com>
2026-08-06 10:17:01 +0000

    Settings: Implement AppLock credential utility and custom authentication support [2/2]
    Change-Id: I597dd5f1f676fe4c57c12f9e68d9b6c1bb049925
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -198,6 +198,7 @@ import com.android.settings.safetycenter.MoreSecurityPrivacyFragment;
 import com.android.settings.safetycenter.ui.SafetyCenterFragment;
 import com.android.settings.safetycenter.ui.SafetyCenterQsFragment;
 import com.android.settings.security.AppLockSettingsFragment;
+import com.android.settings.security.AppLockSubSettingsFragment;
 import com.android.settings.security.ContentProtectionPreferenceFragment;
 import com.android.settings.security.LockscreenDashboardFragment;
 import com.android.settings.security.MemtagPage;
@@ -434,6 +435,7 @@ public class SettingsGateway {
             ContactsStorageSettings.class.getName(),
             ContentProtectionPreferenceFragment.class.getName(),
             AppLockSettingsFragment.class.getName(),
+            AppLockSubSettingsFragment.class.getName(),
             MagnificationPreferenceFragment.class.getName(),
             FlashNotificationsPreferenceFragment.class.getName(),
             BundlePreferenceFragment.class.getName(),
