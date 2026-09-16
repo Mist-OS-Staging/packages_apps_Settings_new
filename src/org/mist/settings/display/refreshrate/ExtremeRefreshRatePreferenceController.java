Evolution-X/packages_apps_Settings

Settings: Implement RefreshRateManager [SQUASHED] [2/2]
Includes:
- Per-app refresh rate config
- Extreme refresh rate (Force all apps to run in maximum refresh rate)
- Temp refresh rate allowed (Used in MEMC)
- Refresh rate QS tile

Change-Id: I30024eb8ce63f00a6f2fa1ea0230b004ff93ac74
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Create BasePerAppConfigFragment

* Allow more preference types instead of only ListPreference.

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Create PerAppSwitchConfigFragment

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Add top info for per-app settings

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Don't show system apps for sensor block fragment

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Pass uid in per-app config fragment

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Allow specific allowed system app list for per-app fragment

* We need to allow dialer/messages/contact for fullscreen display settings.

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Make sure per-app list has no duplicates

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Warn user app reload before switching fullscreen display

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Adapt for Android U QPR2

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings: Use ArrayMap/Set instead of HashMap/Set

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

[joeyhuab: Squashed all changes by same author]

Settings: Add null check for PreferenceScreen in BasePerAppConfigFragment

Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

src/org/evolution/settings/display/refreshrate/ExtremeRefreshRatePreferenceController.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.settings.display.refreshrate;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+import org.evolution.display.RefreshRateManager;
+
+public class ExtremeRefreshRatePreferenceController extends TogglePreferenceController {
+
+    private final RefreshRateManager mRefreshRateManager;
+
+    public ExtremeRefreshRatePreferenceController(Context context, String key) {
+        super(context, key);
+        mRefreshRateManager = context.getSystemService(RefreshRateManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                EXTREME_REFRESH_RATE, 0, UserHandle.USER_CURRENT) == 1;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        if (!isChecked) {
+            mRefreshRateManager.setExtremeRefreshRateEnabled(false);
+            return true;
+        }
+
+        new AlertDialog.Builder(mContext)
+                .setTitle(mContext.getText(R.string.confirm_before_enable_title))
+                .setMessage(mContext.getText(R.string.extreme_refresh_rate_warning))
+                .setPositiveButton(R.string.okay, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mRefreshRateManager.setExtremeRefreshRateEnabled(true);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+        return isChecked();
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return 0;
+    }
+}

src/org/evolution/settings/display/refreshrate/MinRefreshRatePreferenceController.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ * Copyright (C) 2022-2024 Nameless-AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evolution.settings.display.refreshrate;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.util.evolution.DisplayRefreshRateHelper;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinRefreshRatePreferenceController extends BasePreferenceController
+        implements Preference.OnPreferenceChangeListener {
+
+    private final ArrayList<Integer> mSupportedList;
+    private final DisplayRefreshRateHelper mHelper;
+
+    private ListPreference mListPreference;
+
+    public MinRefreshRatePreferenceController(Context context, String key) {
+        super(context, key);
+
+        mHelper = DisplayRefreshRateHelper.getInstance(context);
+        mSupportedList = mHelper.getSupportedRefreshRateList();
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mSupportedList.size() > 1 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+
+        mListPreference = screen.findPreference(getPreferenceKey());
+
+        final List<String> entries = new ArrayList<>();
+        final List<String> values = new ArrayList<>();
+
+        for (int i = 0; i < mSupportedList.size(); ++i) {
+            final String refreshRate = String.valueOf(mSupportedList.get(i));
+            entries.add(refreshRate + " Hz");
+            values.add(refreshRate);
+        }
+
+        mListPreference.setEntries(entries.toArray(new String[entries.size()]));
+        mListPreference.setEntryValues(values.toArray(new String[values.size()]));
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final String refreshRate = String.valueOf(mHelper.getMinimumRefreshRate());
+        final int index = mListPreference.findIndexOfValue(refreshRate);
+        if (index != -1) {
+            mListPreference.setValueIndex(index);
+            mListPreference.setSummary(mListPreference.getEntries()[index]);
+        } else {
+            mListPreference.setSummary(refreshRate + " Hz");
+        }
+
+        final int minRefreshRate = Integer.parseInt(refreshRate);
+        final int peakRefreshRate = mHelper.getPeakRefreshRate();
+        if (peakRefreshRate < minRefreshRate) {
+            mHelper.setPeakRefreshRate(minRefreshRate);
+        }
+
+        final boolean extremeMode = Settings.System.getIntForUser(
+                mContext.getContentResolver(), EXTREME_REFRESH_RATE,
+                0, UserHandle.USER_CURRENT) != 0;
+        mListPreference.setEnabled(!extremeMode);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        mHelper.setMinimumRefreshRate(Integer.parseInt((String) newValue));
+        updateState(preference);
+        return true;
+    }
+}

src/org/evolution/settings/display/refreshrate/PerAppRefreshRatePreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.settings.display.refreshrate;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.util.evolution.DisplayRefreshRateHelper;
+
+import com.android.settings.core.BasePreferenceController;
+
+public class PerAppRefreshRatePreferenceController extends BasePreferenceController {
+
+    private final DisplayRefreshRateHelper mHelper;
+
+    private Preference mPreference;
+
+    public PerAppRefreshRatePreferenceController(Context context, String key) {
+        super(context, key);
+        mHelper = DisplayRefreshRateHelper.getInstance(context);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mHelper.getSupportedRefreshRateList().size() > 1
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final boolean extremeMode = Settings.System.getIntForUser(
+                mContext.getContentResolver(), EXTREME_REFRESH_RATE,
+                0, UserHandle.USER_CURRENT) != 0;
+        mPreference.setEnabled(!extremeMode);
+    }
+}

src/org/evolution/settings/display/refreshrate/ScreenRefreshRateFragment.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.settings.display.refreshrate;
+
+import static android.provider.Settings.System.MIN_REFRESH_RATE;
+import static android.provider.Settings.System.PEAK_REFRESH_RATE;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.app.settings.SettingsEnums;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+
+public class ScreenRefreshRateFragment extends DashboardFragment {
+
+    private static final String TAG = "ScreenRefreshRateFragment";
+
+    private final SettingsObserver mObserver = new SettingsObserver();
+
+    private final class SettingsObserver extends ContentObserver {
+
+        private final Uri mMinUri = Settings.System.getUriFor(MIN_REFRESH_RATE);
+        private final Uri mPeakUri = Settings.System.getUriFor(PEAK_REFRESH_RATE);
+        private final Uri mExtremeUri = Settings.System.getUriFor(EXTREME_REFRESH_RATE);
+
+        SettingsObserver() {
+            super(new Handler());
+        }
+
+        void register(ContentResolver cr) {
+            cr.registerContentObserver(mMinUri, false, this);
+            cr.registerContentObserver(mPeakUri, false, this);
+            cr.registerContentObserver(mExtremeUri, false, this);
+        }
+
+        void unregister(ContentResolver cr) {
+            cr.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            updatePreferenceStates();
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        mObserver.register(getContext().getContentResolver());
+    }
+
+    @Override
+    public void onDestroy() {
+        mObserver.unregister(getContext().getContentResolver());
+        super.onDestroy();
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.PAGE_UNKNOWN;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.screen_refresh_rate;
+    }
+}

src/org/evolution/settings/fragment/PerAppListConfigFragment.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.evolution.settings.fragment;
+
+import android.content.Context;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import java.util.List;
+
+import org.evolution.settings.widget.AppListPreference;
+
+public abstract class PerAppListConfigFragment extends BasePerAppConfigFragment {
+
+    @Override
+    protected Preference createAppPreference(Context prefContext, AppData appData) {
+        final ListPreference pref = new AppListPreference(prefContext);
+        pref.setIcon(getIcon(appData.packageName));
+        pref.setTitle(appData.label);
+        pref.setDialogTitle(appData.label);
+
+        int size = getEntries().size();
+
+        CharSequence[] entries = new CharSequence[size];
+        CharSequence[] values = new CharSequence[size];
+        for (int i = 0; i < size; ++i) {
+            entries[i] = getEntries().get(i);
+            values[i] = String.valueOf(getValues().get(i));
+        }
+        pref.setEntries(entries);
+        pref.setEntryValues(values);
+
+        final String value = String.valueOf(getCurrentValue(appData.packageName, appData.uid));
+        CharSequence summary = "-1";
+        for (int i = 0; i < size; ++i) {
+            if (values[i].equals(value)) {
+                summary = entries[i];
+                break;
+            }
+        }
+        pref.setValue(value);
+        pref.setSummary(summary);
+
+        pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                final String newValueStr = String.valueOf(newValue);
+                onValueChanged(appData.packageName, appData.uid, Integer.parseInt(newValueStr));
+                CharSequence[] entries = pref.getEntries();
+                CharSequence[] values = pref.getEntryValues();
+                CharSequence summary = entries[0];
+                for (int i = 0; i < entries.length; ++i) {
+                    if (values[i].equals(newValueStr)) {
+                        summary = entries[i];
+                        break;
+                    }
+                }
+                pref.setSummary(summary);
+                return true;
+            }
+        });
+
+        return pref;
+    }
+
+    protected abstract List<String> getEntries();
+
+    protected abstract List<Integer> getValues();
+
+    protected abstract int getCurrentValue(String packageName, int uid);
+
+    protected abstract void onValueChanged(String packageName, int uid, int value);
+}

Mist-OS-Staging/packages_apps_Settings_new

src/org/mist/settings/display/refreshrate/ExtremeRefreshRatePreferenceController.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.mist.settings.display.refreshrate;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+import org.mist.display.RefreshRateManager;
+
+public class ExtremeRefreshRatePreferenceController extends TogglePreferenceController {
+
+    private final RefreshRateManager mRefreshRateManager;
+
+    public ExtremeRefreshRatePreferenceController(Context context, String key) {
+        super(context, key);
+        mRefreshRateManager = context.getSystemService(RefreshRateManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                EXTREME_REFRESH_RATE, 0, UserHandle.USER_CURRENT) == 1;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        if (!isChecked) {
+            mRefreshRateManager.setExtremeRefreshRateEnabled(false);
+            return true;
+        }
+
+        new AlertDialog.Builder(mContext)
+                .setTitle(mContext.getText(R.string.confirm_before_enable_title))
+                .setMessage(mContext.getText(R.string.extreme_refresh_rate_warning))
+                .setPositiveButton(R.string.okay, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mRefreshRateManager.setExtremeRefreshRateEnabled(true);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+        return isChecked();
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return 0;
+    }
+}

src/org/mist/settings/display/refreshrate/MinRefreshRatePreferenceController.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ * Copyright (C) 2022-2024 Nameless-AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mist.settings.display.refreshrate;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.util.mist.DisplayRefreshRateHelper;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinRefreshRatePreferenceController extends BasePreferenceController
+        implements Preference.OnPreferenceChangeListener {
+
+    private final ArrayList<Integer> mSupportedList;
+    private final DisplayRefreshRateHelper mHelper;
+
+    private ListPreference mListPreference;
+
+    public MinRefreshRatePreferenceController(Context context, String key) {
+        super(context, key);
+
+        mHelper = DisplayRefreshRateHelper.getInstance(context);
+        mSupportedList = mHelper.getSupportedRefreshRateList();
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mSupportedList.size() > 1 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+
+        mListPreference = screen.findPreference(getPreferenceKey());
+
+        final List<String> entries = new ArrayList<>();
+        final List<String> values = new ArrayList<>();
+
+        for (int i = 0; i < mSupportedList.size(); ++i) {
+            final String refreshRate = String.valueOf(mSupportedList.get(i));
+            entries.add(refreshRate + " Hz");
+            values.add(refreshRate);
+        }
+
+        mListPreference.setEntries(entries.toArray(new String[entries.size()]));
+        mListPreference.setEntryValues(values.toArray(new String[values.size()]));
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final String refreshRate = String.valueOf(mHelper.getMinimumRefreshRate());
+        final int index = mListPreference.findIndexOfValue(refreshRate);
+        if (index != -1) {
+            mListPreference.setValueIndex(index);
+            mListPreference.setSummary(mListPreference.getEntries()[index]);
+        } else {
+            mListPreference.setSummary(refreshRate + " Hz");
+        }
+
+        final int minRefreshRate = Integer.parseInt(refreshRate);
+        final int peakRefreshRate = mHelper.getPeakRefreshRate();
+        if (peakRefreshRate < minRefreshRate) {
+            mHelper.setPeakRefreshRate(minRefreshRate);
+        }
+
+        final boolean extremeMode = Settings.System.getIntForUser(
+                mContext.getContentResolver(), EXTREME_REFRESH_RATE,
+                0, UserHandle.USER_CURRENT) != 0;
+        mListPreference.setEnabled(!extremeMode);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        mHelper.setMinimumRefreshRate(Integer.parseInt((String) newValue));
+        updateState(preference);
+        return true;
+    }
+}

src/org/mist/settings/display/refreshrate/PerAppRefreshRatePreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.mist.settings.display.refreshrate;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.util.mist.DisplayRefreshRateHelper;
+
+import com.android.settings.core.BasePreferenceController;
+
+public class PerAppRefreshRatePreferenceController extends BasePreferenceController {
+
+    private final DisplayRefreshRateHelper mHelper;
+
+    private Preference mPreference;
+
+    public PerAppRefreshRatePreferenceController(Context context, String key) {
+        super(context, key);
+        mHelper = DisplayRefreshRateHelper.getInstance(context);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mHelper.getSupportedRefreshRateList().size() > 1
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mPreference = screen.findPreference(getPreferenceKey());
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final boolean extremeMode = Settings.System.getIntForUser(
+                mContext.getContentResolver(), EXTREME_REFRESH_RATE,
+                0, UserHandle.USER_CURRENT) != 0;
+        mPreference.setEnabled(!extremeMode);
+    }
+}

src/org/mist/settings/display/refreshrate/ScreenRefreshRateFragment.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.mist.settings.display.refreshrate;
+
+import static android.provider.Settings.System.MIN_REFRESH_RATE;
+import static android.provider.Settings.System.PEAK_REFRESH_RATE;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.app.settings.SettingsEnums;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+
+public class ScreenRefreshRateFragment extends DashboardFragment {
+
+    private static final String TAG = "ScreenRefreshRateFragment";
+
+    private final SettingsObserver mObserver = new SettingsObserver();
+
+    private final class SettingsObserver extends ContentObserver {
+
+        private final Uri mMinUri = Settings.System.getUriFor(MIN_REFRESH_RATE);
+        private final Uri mPeakUri = Settings.System.getUriFor(PEAK_REFRESH_RATE);
+        private final Uri mExtremeUri = Settings.System.getUriFor(EXTREME_REFRESH_RATE);
+
+        SettingsObserver() {
+            super(new Handler());
+        }
+
+        void register(ContentResolver cr) {
+            cr.registerContentObserver(mMinUri, false, this);
+            cr.registerContentObserver(mPeakUri, false, this);
+            cr.registerContentObserver(mExtremeUri, false, this);
+        }
+
+        void unregister(ContentResolver cr) {
+            cr.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            updatePreferenceStates();
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        mObserver.register(getContext().getContentResolver());
+    }
+
+    @Override
+    public void onDestroy() {
+        mObserver.unregister(getContext().getContentResolver());
+        super.onDestroy();
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.PAGE_UNKNOWN;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.screen_refresh_rate;
+    }
+}

src/org/mist/settings/fragment/PerAppListConfigFragment.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.mist.settings.fragment;
+
+import android.content.Context;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import java.util.List;
+
+import org.mist.settings.widget.AppListPreference;
+
+public abstract class PerAppListConfigFragment extends BasePerAppConfigFragment {
+
+    @Override
+    protected Preference createAppPreference(Context prefContext, AppData appData) {
+        final ListPreference pref = new AppListPreference(prefContext);
+        pref.setIcon(getIcon(appData.packageName));
+        pref.setTitle(appData.label);
+        pref.setDialogTitle(appData.label);
+
+        int size = getEntries().size();
+
+        CharSequence[] entries = new CharSequence[size];
+        CharSequence[] values = new CharSequence[size];
+        for (int i = 0; i < size; ++i) {
+            entries[i] = getEntries().get(i);
+            values[i] = String.valueOf(getValues().get(i));
+        }
+        pref.setEntries(entries);
+        pref.setEntryValues(values);
+
+        final String value = String.valueOf(getCurrentValue(appData.packageName, appData.uid));
+        CharSequence summary = "-1";
+        for (int i = 0; i < size; ++i) {
+            if (values[i].equals(value)) {
+                summary = entries[i];
+                break;
+            }
+        }
+        pref.setValue(value);
+        pref.setSummary(summary);
+
+        pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                final String newValueStr = String.valueOf(newValue);
+                onValueChanged(appData.packageName, appData.uid, Integer.parseInt(newValueStr));
+                CharSequence[] entries = pref.getEntries();
+                CharSequence[] values = pref.getEntryValues();
+                CharSequence summary = entries[0];
+                for (int i = 0; i < entries.length; ++i) {
+                    if (values[i].equals(newValueStr)) {
+                        summary = entries[i];
+                        break;
+                    }
+                }
+                pref.setSummary(summary);
+                return true;
+            }
+        });
+
+        return pref;
+    }
+
+    protected abstract List<String> getEntries();
+
+    protected abstract List<Integer> getValues();
+
+    protected abstract int getCurrentValue(String packageName, int uid);
+
+    protected abstract void onValueChanged(String packageName, int uid, int value);
+}