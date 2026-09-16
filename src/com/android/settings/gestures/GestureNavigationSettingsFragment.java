tyty-android/packages_apps_Settings

Settings: gestures: Wire sysui_show_nav_bar_ime to ImeNavBarSpaceOverlay
Adds a preference change listener for the "sysui_show_nav_bar_ime"
switch that toggles com.android.systemui.overlay.imenavbarspace via
OverlayManager, forcing config_imeDrawsImeNavBar to false when the
setting is disabled.

Since NexusLauncher and Launcher3 only read this resource once when
constructing their taskbar context, toggling the overlay alone does
not update the already-running process. Both launcher packages are
restarted after the overlay state changes, when installed, so the
new resource value takes effect immediately rather than only after
their next incidental re-init (e.g. rotation or app switch).

Co-authored-by: Claude Sonnet 5 <noreply@anthropic.com>

src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
@@ -45,6 +45,7 @@ import java.util.Locale;
 
 import lineageos.preference.LineageSystemSettingSwitchPreference;
 
+import org.evolution.settings.preferences.SecureSettingSwitchPreference;
 import org.evolution.settings.utils.DeviceUtils;
 
 /**
@@ -74,6 +75,7 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
     private static final String NOGESTUREHINT_OVERLAY = "com.google.android.apps.nexuslauncher.overlay.nogesturehint";
     private static final String LAUNCHER3_PACKAGE_NAME = "com.android.launcher3";
     private static final String LAUNCHER3_NOGESTUREHINT_OVERLAY = "com.android.launcher3.overlay.nogesturehint";
+    private static final String IME_NAV_BAR_SPACE_OVERLAY = "com.android.systemui.overlay.imenavbarspace";
 
     private WindowManager mWindowManager;
     private BackGestureIndicatorView mIndicatorView;
@@ -122,6 +124,21 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
             }
             if (Utils.isPackageInstalled(getContext(), LAUNCHER3_PACKAGE_NAME)) {
                 Utils.toggleOverlay(getContext(), LAUNCHER3_NOGESTUREHINT_OVERLAY, !(Boolean) newValue);
+                Utils.restartApp(LAUNCHER3_PACKAGE_NAME, getContext());
+            }
+
+            return true;
+        });
+
+        SecureSettingSwitchPreference showImeSpacePref =
+                getPreferenceScreen().findPreference("sysui_show_nav_bar_ime");
+
+        showImeSpacePref.setOnPreferenceChangeListener((preference, newValue) -> {
+            Utils.toggleOverlay(getContext(), IME_NAV_BAR_SPACE_OVERLAY, !(Boolean) newValue);
+            if (Utils.isPackageInstalled(getContext(), NEXUSLAUNCHER_PACKAGE_NAME)) {
+                Utils.restartApp(NEXUSLAUNCHER_PACKAGE_NAME, getContext());
+            }
+            if (Utils.isPackageInstalled(getContext(), LAUNCHER3_PACKAGE_NAME)) {
                 Utils.restartApp(LAUNCHER3_PACKAGE_NAME, getContext());
             }
 

Mist-OS-Staging/packages_apps_Settings_new

src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
@@ -45,6 +45,7 @@ import java.util.Locale;
 
 import lineageos.preference.LineageSystemSettingSwitchPreference;
 
+import org.mist.settings.preferences.SecureSettingSwitchPreference;
 import org.mist.settings.utils.DeviceUtils;
 
 /**
@@ -74,6 +75,7 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
     private static final String NOGESTUREHINT_OVERLAY = "com.google.android.apps.nexuslauncher.overlay.nogesturehint";
     private static final String LAUNCHER3_PACKAGE_NAME = "com.android.launcher3";
     private static final String LAUNCHER3_NOGESTUREHINT_OVERLAY = "com.android.launcher3.overlay.nogesturehint";
+    private static final String IME_NAV_BAR_SPACE_OVERLAY = "com.android.systemui.overlay.imenavbarspace";
 
     private WindowManager mWindowManager;
     private BackGestureIndicatorView mIndicatorView;
@@ -122,6 +124,21 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
             }
             if (Utils.isPackageInstalled(getContext(), LAUNCHER3_PACKAGE_NAME)) {
                 Utils.toggleOverlay(getContext(), LAUNCHER3_NOGESTUREHINT_OVERLAY, !(Boolean) newValue);
+                Utils.restartApp(LAUNCHER3_PACKAGE_NAME, getContext());
+            }
+
+            return true;
+        });
+
+        SecureSettingSwitchPreference showImeSpacePref =
+                getPreferenceScreen().findPreference("sysui_show_nav_bar_ime");
+
+        showImeSpacePref.setOnPreferenceChangeListener((preference, newValue) -> {
+            Utils.toggleOverlay(getContext(), IME_NAV_BAR_SPACE_OVERLAY, !(Boolean) newValue);
+            if (Utils.isPackageInstalled(getContext(), NEXUSLAUNCHER_PACKAGE_NAME)) {
+                Utils.restartApp(NEXUSLAUNCHER_PACKAGE_NAME, getContext());
+            }
+            if (Utils.isPackageInstalled(getContext(), LAUNCHER3_PACKAGE_NAME)) {
                 Utils.restartApp(LAUNCHER3_PACKAGE_NAME, getContext());
             }
 